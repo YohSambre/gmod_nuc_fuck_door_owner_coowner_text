e5f7cf9f754506afbadca16d4fac9abb49e97c81
YohSambre <YohSambre@users.noreply.github.com>
2024-10-01 11:52:07 +0200

    Add files via upload

---
+++ nuc_fuck_door_owner_coowner_text/lua/autorun/nuc_fdocoot_sv.lua
@@ -0,0 +1,13 @@
+--------------------------------------------
+--*/ NUC JOIN THE BATTLE
+--------------------------------------------
+MsgC (Color(255, 255, 255), [[                                                                                                              
+= NUC - Fuck Door Names of owner and co-owner players loaded!
+]])
+
+--------------------------------------------
+--*/ BASE
+--------------------------------------------
+hook.Add("HUDDrawDoorData", "NUC_FUCK_DOOR_OWNER_COOWNER_TEXT", function() 
+	return true 
+end)
